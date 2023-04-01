3e7c26e5ef84107c5d64d6cbdf28f4d59d48fa41
Pratikkk <kenipratik9@gmail.com>
2023-04-01 15:42:07 +0530

    set form fields

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,12 +1,11 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+
 
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
 } from '../../utils/firebase/firebase-utils.js';
+
+import SignUp from '../../components/sign-up/sign-up.jsx';
 
 const SignIn = () => {
   
@@ -19,6 +18,7 @@ const SignIn = () => {
     <>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUp> </SignUp>
     </>
   );
 };
