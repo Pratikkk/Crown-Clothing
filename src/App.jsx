a5d2e1463b6aedc9f038841fa52828f8438f6512
Pratikkk <kenipratik9@gmail.com>
2023-04-01 10:01:36 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./catogories-styles.scss";
+import Directory from "./components/directory/Directory.jsx";
 
 function App() {
   const categories = [
@@ -19,7 +20,7 @@ function App() {
     },
     {
       id: 4,
-      title: "womens",
+      title: "women's",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
@@ -30,20 +31,7 @@ function App() {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+  <Directory categories={categories}/>
   );
 }
 export default App;
