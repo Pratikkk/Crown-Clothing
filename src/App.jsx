1e43c32435250d55d232d53b9b74cb28843353be
Pratikkk <kenipratik9@gmail.com>
2023-04-01 12:02:38 +0530

    logo and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./catogories-styles.scss";
 import HomeComponent from "./routes/home/HomeComponent.jsx";
-import { Routes, Route, Outlet } from "react-router-dom";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h2>I am the navigation bar</h2>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation.jsx";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
